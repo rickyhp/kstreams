e595aab11324a54ab0cdfd94be9fd034dbaef76d
rputra <rhpmail@gmail.com>
2018-11-11 12:22:30 +0700

    fix deprecated methods

--- src/main/java/com/rickyhp/kstreams/App.java
+++ src/main/java/com/rickyhp/kstreams/App.java
@@ -3,13 +3,10 @@ package com.rickyhp.kstreams;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KStreamBuilder;
-import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Produced;
 import org.apache.kafka.streams.Consumed;
-import java.util.Arrays;
 import java.util.Properties;
 import com.rickyhp.model.Purchase;
 import com.rickyhp.serializer.JsonDeserializer;
@@ -26,20 +23,15 @@ public class App
         
         Serde<String> stringSerde = Serdes.String();
         Serde<Purchase> purchaseSerde = Serdes.serdeFrom(purchaseJsonSerializer,purchaseJsonDeserializer);
+         
+        StreamsBuilder builder = new StreamsBuilder();
+        builder.<String, Purchase>stream("src-topic", Consumed.with(stringSerde, purchaseSerde))
+        	.mapValues(value -> Purchase.builder(value).maskCreditCard().build())
+        	.to("purchases", Produced.with(stringSerde, purchaseSerde));
         
-        KStreamBuilder kStreamBuilder = new KStreamBuilder();
-
-
-        KStream<String,Purchase> purchaseKStream = kStreamBuilder.stream(stringSerde,purchaseSerde,"src-topic")
-                .mapValues(p -> Purchase.builder(p).maskCreditCard().build());
-        
-        purchaseKStream.to(stringSerde,purchaseSerde,"purchases");
-
-        System.out.println("Starting PurchaseStreams Example");
-        KafkaStreams kafkaStreams = new KafkaStreams(kStreamBuilder,streamingConfig);
+        KafkaStreams kafkaStreams = new KafkaStreams(builder.build(),streamingConfig);
         kafkaStreams.cleanUp();
         kafkaStreams.start();
-        System.out.println("Now started PurchaseStreams Example");
 
     }
 
@@ -49,9 +41,7 @@ public class App
         props.put("group.id", "kstreams1-consumer-group");
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "testing-processor-api");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        //props.put(StreamsConfig.ZOOKEEPER_CONNECT_CONFIG, "localhost:2181");
         props.put(StreamsConfig.REPLICATION_FACTOR_CONFIG, 1);
-        //props.put(StreamsConfig.TIMESTAMP_EXTRACTOR_CLASS_CONFIG, WallclockTimestampExtractor.class);
         return props;
     }
 }
